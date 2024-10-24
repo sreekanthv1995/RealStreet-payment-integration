64076f176c2ed52966258eba415fc252603fbaa9
Sreekanth V <sreekanthv1995@gmail.com>
2024-10-24 12:05:46 +0530

    setup-project

---
+++ src/main/java/com/realstreet_payment_integration/realstreet/RealStreetApplication.java
@@ -0,0 +1,13 @@
+package com.realstreet_payment_integration.realstreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealStreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealStreetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realstreet_payment_integration/realstreet/RealstreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realstreet_payment_integration.realstreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealstreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
